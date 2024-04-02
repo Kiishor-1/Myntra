ae6af22fae170ab9468261ba9d7aa950e2ddae38
Kishor Nishad <kishorr127.0@gmail.com>
2024-04-02 19:48:01 +0530

    InitializeUser

--- src/Slices/authSlice.js
+++ src/Slices/authSlice.js
@@ -10,14 +10,23 @@ const authSlice = createSlice({
   name: "auth",
   initialState: initialState,
   reducers: {
-    setSignupData(state, value) {
-      state.signupData = value.payload;
+    // setSignupData(state, value) {
+    //   state.signupData = value.payload;
+    // },
+    // setLoading(state, value) {
+    //   state.loading = value.payload;
+    // },
+    // setToken(state, value) {
+    //   state.token = value.payload;
+    // },
+    setSignupData(state, action) { // Changed 'value' to 'action'
+      state.signupData = action.payload; // Directly set to action.payload
     },
-    setLoading(state, value) {
-      state.loading = value.payload;
+    setLoading(state, action) { // Changed 'value' to 'action'
+      state.loading = action.payload;
     },
-    setToken(state, value) {
-      state.token = value.payload;
+    setToken(state, action) { // Changed 'value' to 'action'
+      state.token = action.payload;
     },
   },
 });
